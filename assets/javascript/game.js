15b561fef89f089e602ff8732d02df3a2211406b
Torian <tharris@zyrobotics.com>
2018-12-03 16:39:10 -0500

    added hidden word functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,78 @@
-
-
-let word = "shadowverse";
-
-
-
-//function someFunc() { return longFunc().then(...); };
-// var pr = someFunc(); 
-// pr.then(function(data) { console.log(data) });
-//console.log (getPokemonNames());
-
 let game = {
+    currentWord: "",
+    hiddenWord: "",
     words: [],
-    getPokemonNames: function() {
+    isPlaying: true,
+
+    getPokemonNames: function () {
+        //create new instance of pokeapi's js wrapper, retreive the pokemon_species
+        // value from it, then send that value to fillWords()
         let P = new Pokedex.Pokedex({ protocol: 'https' });
-        let w;
-        //for (let i = 0; i < 151; i++) {
-            P.getGenerationByName("generation-i").then(function (response) {
-                    console.log(response.pokemon_species[0].name);
-                    setName(response.pokemon_species[0].name);
-                    setName(response.pokemon_species[1].name);
-                })
-        //}
+        P.getGenerationByName("generation-i").then(function (response) {
+            game.fillWords(response.pokemon_species);
+        })
     },
-    
-    setName: function (val)
-    {
-        document.getElementById("wordToGuess").innerHTML = val;
-    }, 
-    
+
+    fillWords: function (val) {
+        //loop through value and add the pokemon names to words[];
+        for (let i = 0; i < val.length; i++) {
+            this.words.push(val[i].name);
+        }
+        this.getRandomWord();
+    },
+
     getRandomWord: function () {
-        return this.words[Math.floor(Math.random() * this.words.length)];
+        this.currentWord = this.words[Math.floor(Math.random() * this.words.length)];
+        for (let i = 0; i < this.currentWord.length; i++) {
+            this.hiddenWord += "_";
+        }
+        this.updateWord();
+        console.log(this.currentWord);
+    },
+
+    compareChar: function (char) {
+        let indices = [];
+        let idx = this.currentWord.indexOf(char);
+        while (idx != -1) {
+            indices.push(idx);
+            idx = this.currentWord.indexOf(char, idx + 1);
+        }
+        //this.hiddenWord = this.hiddenWord.replace(this.hiddenWord[idx], char);
+        for(let i = 0; i < indices.length; i++)
+        {
+            function replaceAt(string, index, replace) {
+                return this.hiddenWord.substring(0, ) + replace + string.substring(index + 1);
+              }
+        }
+
+        this.hiddenWord = this.hiddenWord.replace(this.hiddenWord[indices[i]], char);
+        console.log(indices);
+        console.log(this.hiddenWord);
+        this.updateWord();
+        //return indices;
+        // [0, 2, 4]
+    },
+
+    updateWord: function () {
+        document.getElementById("wordToGuess").innerHTML = this.hiddenWord;
     }
 };
-getPokemonNames();
-//console.log(game.getPokemonNames());
-//console.log(game.words);
-// document.getElementById("wordToGuess").innerHTML = response.pokemon_species[0].name;
+
+document.onkeydown = function () {
+    let key = event.key;
+    if (game.isPlaying) {
+        // if (game.compareChar(key) > 0)
+        //     alert("you got a letter");
+        // else
+        //     alert("you didnt get a letter");
+        game.compareChar(key);
+
+    }
+    else {
+
+    }
+}
+
+
+game.getPokemonNames();
+
