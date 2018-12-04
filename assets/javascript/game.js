d4b1661195042f756f32575a18ba3baa6c946515
Shaun <torianharris917@gmail.com>
2018-12-03 23:26:17 -0500

    game functions finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
 let game = {
     currentWord: "",
     hiddenWord: "",
+    missedLetters: "",
+    stateMessage: "",
+    wins: 0,
+    loses: 0,
+    startGuesses: 10,
+    guessesLeft: 0,
     words: [],
     isPlaying: true,
 
@@ -18,7 +24,7 @@ let game = {
         for (let i = 0; i < val.length; i++) {
             this.words.push(val[i].name);
         }
-        this.getRandomWord();
+        this.startGame();
     },
 
     getRandomWord: function () {
@@ -26,7 +32,7 @@ let game = {
         for (let i = 0; i < this.currentWord.length; i++) {
             this.hiddenWord += "_";
         }
-        this.updateWord();
+        this.updateDoc();
         console.log(this.currentWord);
     },
 
@@ -38,32 +44,65 @@ let game = {
             idx = this.currentWord.indexOf(char, idx + 1);
         }
 
-        for(let i = 0; i < indices.length; i++)
-        {
-            this.hiddenWord = this.hiddenWord.substring(0, indices[i]) + 
-            char + this.hiddenWord.substring(indices[i] + 1);
+        for (let i = 0; i < indices.length; i++) {
+            this.hiddenWord = this.hiddenWord.substring(0, indices[i]) +
+                char + this.hiddenWord.substring(indices[i] + 1);
         }
-        
-        this.updateWord();
+        return indices.length;
     },
 
-    updateWord: function () {
+    updateDoc: function () {
+        this.checkGameState();
+        document.getElementById("winScore").innerHTML = "Wins: " + this.wins;
+        document.getElementById("loseScore").innerHTML = "Loses: " + this.loses;
+        document.getElementById("guessesRemaining").innerHTML = "Guesses Left: " + this.guessesLeft;
         document.getElementById("wordToGuess").innerHTML = this.hiddenWord;
+        document.getElementById("guessedLetters").innerHTML = this.missedLetters;
+        document.getElementById("endGameMessage").innerHTML = this.stateMessage;
+
+    },
+
+    checkGameState: function () {
+        if (this.hiddenWord === this.currentWord)
+        {
+            game.stateMessage = "You Win! Press any key to play again.";
+            game.wins++;
+            this.isPlaying = false;
+        }
+
+        if (this.guessesLeft === 0)
+        {
+            game.stateMessage = "Better luck next time... Press any key to try again."
+            game.loses++;
+            this.isPlaying = false;
+        }
+    },
+
+    startGame: function () {
+        this.guessesLeft = this.startGuesses;
+        this.hiddenWord = "";
+        this.missedLetters = "";
+        this.stateMessage = "";
+        this.isPlaying = true;
+        this.getRandomWord();
     }
 };
 
 document.onkeydown = function () {
     let key = event.key;
     if (game.isPlaying) {
-        // if (game.compareChar(key) > 0)
-        //     alert("you got a letter");
-        // else
-        //     alert("you didnt get a letter");
-        game.compareChar(key);
-
+        // if the key isn't found in currentWord and the key isn't in missingLetters...
+        if (game.compareChar(key) === 0 && game.missedLetters.indexOf(key) === -1) {
+            if (game.missedLetters.length === 0)
+                game.missedLetters += key;
+            else
+                game.missedLetters += ", " + key;
+            game.guessesLeft --;
+        }
+        game.updateDoc();
     }
     else {
-
+        game.startGame();
     }
 }
 
